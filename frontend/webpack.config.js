ac69456b0ade97a2d261c5670e295dd15d01f51e
vagabond <vagabond@DESKTOP-AGVK968.localdomain>
2021-04-28 07:31:46 -0400

    init react
    added a fronten using react
    rendered on server through react dom and webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  stats: {
+    // Configure the console output
+    errorDetails: true, //this does show errors
+    colors: true,
+    modules: true,
+    reasons: true
+    },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+};
