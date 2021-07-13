5ae2a735b80df6214dad51a5955f02d9e6a08329
vagabond <vagabond@DESKTOP-AGVK968.localdomain>
2021-07-13 08:51:11 -0400

    minor changes

---
+++ backend/spotify/credentials.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#CLIENT_ID = os.environ.get('CLIENT_ID')
+#CLIENT_SECRET = os.environ.get('ClIENT_SECRET')
+
+CLIENT_ID="1027a93605e54517a41a802246356e12"
+CLIENT_SECRET="1fb8e3a757d84fff8871e3914f825c01"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
